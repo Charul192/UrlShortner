e9ac007591dcf4e2852f5509d2fc6375c1062ed7
Charul192 <charul1902@gmail.com>
2025-06-30 22:02:16 +0530

    Update index.js
    Setup MongodB

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const URL = require('./models/url');
 const app = express();
 const PORT = 8001;
 
-connectToMongoDB("mongodb://127.0.0.1:27017/short-url").then(() => console.log("MongoDB connected"));
+connectToMongoDB("<your-host>:<port>/<your-database-name>").then(() => console.log("MongoDB connected"));
 
 app.use(express.json());
 
